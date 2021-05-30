027e0ac490a71a08ad80896d8abaa44c67500162
Troy Neild <troyneild@Fords-MacBook-Pro-2.local>
2021-05-30 18:50:23 -0400

    Added request and injected into html :D

--- client/java.js
+++ client/java.js
@@ -1,3 +1,23 @@
-var userName = $("name").val()
-var Intrests = $("intrests").val()
+document.onload = onloadfunction();
 
+function onloadfunction() {
+  console.log("Loaded!");
+  const promise = fetch("http://localhost:3000/hello")
+    .then((res) => {
+      console.log(res);
+      return res.json();
+    })
+    .then((res) => {
+      const resTwo = JSON.stringify(res);
+      $("#response").text(resTwo);
+    });
+  console.log(promise);
+}
+
+var userName = $("name").val();
+var Intrests = $("intrests").val();
+
+// const testImport = () => {
+//   console.log("test import");
+// };
+// export default testImport;
